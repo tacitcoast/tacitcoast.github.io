f0c1c2fbd9169514f754bfd3160777524b33ca95
tacitcoast <tacitcoast@gmail.com>
2022-12-22 23:35:15 +0300

    fix js

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('If it's later than 10 p.m. and you think it's still a great idea, don't do it 😉')
+	alert("If it's later than 10 p.m. and you think it's still a great idea, don't do it 😉")
 })
